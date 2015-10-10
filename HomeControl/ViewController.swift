e87d62bfc87a31ec918a7cbd9cba0d3b2c164b9c
Christoph Eicke <christoph.eicke@fortytools.com>
2015-10-10 10:40:21 +0200

    Initial Commit

---
+++ HomeControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeControl
+//
+//  Created by Christoph Eicke on 10.10.15.
+//  Copyright © 2015 Christoph Eicke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
