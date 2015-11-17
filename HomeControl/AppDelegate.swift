c1cdcc1ea0ed057798adc415f1f577eb0c1f0a75
Christoph Eicke <christoph.eicke@fortytools.com>
2015-11-17 20:46:23 +0100

    WIP

--- HomeControl/AppDelegate.swift
+++ HomeControl/AppDelegate.swift
@@ -45,7 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
     
     lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "org.geisterstunde.photastic3" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1]
         }()
