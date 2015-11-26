7d9786300dd74b308ae0ae78ecb3802d3db22966
Christoph Eicke <christoph.eicke@fortytools.com>
2015-11-26 16:10:18 +0100

    simple UI coloring algorithm.

--- HomeControl WatchKit Extension/InterfaceController.swift
+++ HomeControl WatchKit Extension/InterfaceController.swift
@@ -134,7 +134,7 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
             if let row = actorTable.rowControllerAtIndex(index) as? ActorRowController {
                 row.actorName.setText(actorName)
                 
-                let alphaValue = 1 - Double(abs(Double(index) - Double(actorArray.count - 1) / 2 ) / Double(actorArray.count / 2))
+                let alphaValue = 1.2 - Double(abs(Double(index) - Double(actorArray.count - 1) / 2 ) / Double(actorArray.count / 2))
                 
                 let color : UIColor = UIColor.init(red: 0, green: 0.53, blue: 0.46, alpha: CGFloat(alphaValue))
                 row.actorGroup.setBackgroundColor(color)
